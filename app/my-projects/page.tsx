41ca10e844ce24e7079c1a9b085d6064bde0f285
Arun <arunraju9837@gmail.com>
2024-02-05 20:28:25 +0530

    FIX :: Create Pages for navigation

---
+++ app/my-projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>my-projects</div>
+  )
+}
+
+export default page
---
+++ app/my-skills/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>my-skills</div>
+  )
+}
+
+export default page
