63f56fccc068c0a445a80a75039a84ebafb4f4e7
Arun <arunraju9837@gmail.com>
2024-02-05 20:27:19 +0530

    FIX :: Create Transition component

---
+++ components/Navigation.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { NavLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Transition from "./Transition";
+
+function Navigation() {
+  const [isRouting, setIsRouting] = useState(false);
+  const path = usePathname();
+  const [prevPath, setPrevPath] = useState("/");
+
+  useEffect(() => {
+    if (prevPath !== path) {
+      setIsRouting(true);
+    }
+  }, [path, prevPath]);
+
+  useEffect(() => {
+    if (isRouting) {
+      setPrevPath(path);
+      const timeOut = setTimeout(() => {
+        setIsRouting(false);
+      }, 1200);
+
+      return () => clearTimeout(timeOut);
+    }
+  }, [isRouting]);
+  return (
+    <div
+      style={{ left: "20%" }}
+      className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border border-white py-7 bg-black px-4"
+    >
+      {isRouting && <Transition />}
+      {NavLinks.map((nav) => (
+        <Link key={nav.name} href={nav.link} className="mb-16 pl-4 min-w-[20%]">
+          <nav.icon
+            className={`w-[24px] h-[24px] ${
+              path === nav.name ? "text-purple-800" : "text-white"
+            }`}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Navigation;
